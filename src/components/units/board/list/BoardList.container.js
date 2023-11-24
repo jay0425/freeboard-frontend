7061acb58a97a58066af57558ceb038c911e9e52
jay0425 <heedev94@gmail.com>
2023-11-24 17:41:38 +0900

    게시글 목록 css 수정, 게시글 목록 클릭 시 상세보기 이동 기능 구현, 게시물 등록하기 버튼 기능 구현

--- src/components/units/board/list/BoardList.container.js
+++ src/components/units/board/list/BoardList.container.js
@@ -1,4 +1,6 @@
 import { gql, useMutation, useQuery } from '@apollo/client';
+import * as S from './BoardList.styles';
+import { useRouter } from 'next/router';
 
 const FETCH_BOARDS = gql`
   query {
@@ -20,6 +22,8 @@ const DELETE_BOARD = gql`
 `;
 
 export default function BoardList() {
+  const router = useRouter();
+
   const { data } = useQuery(FETCH_BOARDS);
   console.log(data);
   console.log(data?.fetchBoards);
@@ -35,23 +39,36 @@ export default function BoardList() {
     });
   };
 
+  const onClickMoveToBoardNew = () => {
+    router.push('/boards/new');
+  };
+
+  const onClickMoveToDetail = (event) => {
+    router.push(`boards/${event.target.id}`);
+  };
+
   return (
     <>
-      {data?.fetchBoards.map((el) => (
-        <div key={el._id}>
-          <span>
-            <input type="checkbox" />
-          </span>
-          <span style={{ margin: '10px' }}>{el._id}</span>
-          <span style={{ margin: '10px' }}>{el.title}</span>
-          <span style={{ margin: '10px' }}>{el.writer}</span>
-          <span>
-            <button id={el._id} onClick={onClickDelete}>
-              삭제
-            </button>
-          </span>
-        </div>
-      ))}
+      <S.Wrapper>
+        <S.BoardsUl>
+          <S.HeadLi>ID</S.HeadLi>
+          <S.HeadLi>제목</S.HeadLi>
+          <S.HeadLi>작성자</S.HeadLi>
+          <S.HeadLi>날짜</S.HeadLi>
+          {data?.fetchBoards.map((el) => (
+            <S.FectchBoardsMap id={el._id} key={el._id} onClick={onClickMoveToDetail}>
+              <S.BodyLi>{String(el._id).slice(-4).toUpperCase()}</S.BodyLi>
+              <S.BodyLi>{el.title}</S.BodyLi>
+              <S.BodyLi>{el.writer}</S.BodyLi>
+              <S.BodyLi>{el.createdAt}</S.BodyLi>
+            </S.FectchBoardsMap>
+          ))}
+        </S.BoardsUl>
+        <S.WriteButton onClick={onClickMoveToBoardNew}>
+          <S.Pencil src="/images/pencil.svg" alt="연필 이미지" />
+          게시물 등록하기
+        </S.WriteButton>
+      </S.Wrapper>
     </>
   );
 }
--- src/components/units/board/list/BoardList.styles.js
+++ src/components/units/board/list/BoardList.styles.js
@@ -7,89 +7,73 @@ export const Wrapper = styled.div`
   transform: translate(-50%, -50%);
 `;
 
-export const CardWrapper = styled.div`
+export const BoardsUl = styled.ul`
   width: 1200px;
-  min-height: 800px;
-  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
 
-  padding: 80px 120px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+
+  list-style: none;
+  text-align: center;
+  padding: 0;
+  margin: 0;
 `;
 
-export const Header = styled.div`
-  border-bottom: 1px solid #bdbdbd;
+export const HeadLi = styled.li`
+  margin: 10px;
+
+  list-style: none;
+  text-align: center;
+
+  display: inline-block;
+  align-items: flex-start;
+
+  width: 23.3%;
+
+  padding: 13px 0px 12px 0px;
 `;
 
-export const AvatarWrapper = styled.div`
+export const FectchBoardsMap = styled.div`
+  color: #4f4f4f;
+  cursor: pointer;
+
+  border-top: 1px solid #bdbdbd;
+
+  padding: 14px 0px;
+`;
+
+export const BodyLi = styled.li`
+  margin: 10px;
+
+  list-style: none;
+  text-align: center;
+
+  display: inline-block;
+
+  width: 23.3%;
+
+  pointer-events: none;
+`;
+
+export const WriteButton = styled.div`
+  color: #000;
+
+  float: right;
+  border: 1px solid #f2f2f2;
+  border-radius: 10px;
+
   display: flex;
   align-items: center;
 
-  margin-bottom: 20px;
-`;
-
-export const Avatar = styled.img`
-  width: 56px;
-  height: 56px;
-
-  margin-right: 12px;
-`;
-
-export const Info = styled.div``;
-
-export const Writer = styled.div`
-  color: #000;
-  font-family: Noto Sans CJK KR;
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 500;
-`;
-
-export const CreatedAt = styled.div`
-  color: #000;
-  font-family: Noto Sans CJK KR;
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-`;
-
-export const Body = styled.div``;
-
-export const Title = styled.div`
-  color: #000;
-  font-family: Noto Sans CJK KR;
-  font-size: 36px;
-  font-style: normal;
-  font-weight: 700;
-
-  margin: 80px 0px 40px 0px;
-`;
-
-export const Contents = styled.div`
-  color: #000;
-  font-family: Noto Sans CJK KR;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-
-  margin-top: 101px;
-`;
-
-export const Button = styled.button`
-  display: inline-flex;
-  height: 45px;
-  padding: 14px 60px;
-  justify-content: center;
-  align-items: center;
   gap: 8px;
-  margin-right: 24px;
 
-  border: 1px solid #bdbdbd;
-  background: #fff;
+  margin-top: 40px;
+  padding: 14px 16px;
 
   cursor: pointer;
+`;
+
+export const Pencil = styled.img`
+  width: 24px;
+  height: 24px;
 `;
