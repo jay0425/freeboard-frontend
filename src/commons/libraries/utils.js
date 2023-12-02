2ec1d5101ac6ee59de0c331b89b444ded994f65c
jay0425 <heedev94@gmail.com>
2023-12-02 16:59:16 +0900

    게시글 수정 기능 구현

--- src/commons/libraries/utils.js
+++
@@ -1,8 +0,0 @@
-export const getDate = (date) => {
-  //   _ 는 '임시'라는 뜻. 조작한 데이터를 의미함.
-  const _date = new Date(date);
-  const yyyy = _date.getFullYear();
-  const mm = _date.getMonth() + 1;
-  const dd = _date.getDate();
-  return `${yyyy}-${mm}-${dd}`;
-};
